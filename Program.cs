11cc641b030c88ee3c362e61f78c5f46c3903185
Guerra24 <lili@guerra24.net>
2024-06-06 19:26:32 -0600

    Show dialogs if executable is not found or not set

--- Program.cs
+++ Program.cs
@@ -1,71 +1,94 @@
 ﻿using System.Diagnostics;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
 
 namespace GameAwaiter
 {
-	public static class Program
-	{
+    public static class Program
+    {
 
-		public static int Main(string[] args)
-		{
-			if (args.Length == 0)
-			{
-				Console.WriteLine("Path not set");
-				return -1;
-			}
+        public static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Path not set");
+                if (OperatingSystem.IsWindowsVersionAtLeast(10, 0, 10240, 0))
+                    PInvoke.MessageBox(new HWND(0), "Path not set", "GameAwaiter", MESSAGEBOX_STYLE.MB_OK);
+                else
+                {
+                    Console.WriteLine("Path not set");
+                    Console.ReadKey();
+                }
+                return -1;
+            }
 
-			var app = args[0];
-			var delay = args.Length >= 2 ? int.Parse(args[1]) : 5000;
+            var app = args[0];
 
-			if (WaitForNewProcess(app))
-				return 0;
+            if (!File.Exists(app))
+            {
+                if (OperatingSystem.IsWindowsVersionAtLeast(10, 0, 10240, 0))
+                    PInvoke.MessageBox(new HWND(0), $"Executable not found at \"{app}\"", "GameAwaiter", MESSAGEBOX_STYLE.MB_OK);
+                else
+                {
+                    Console.WriteLine($"Executable not found at \"{app}\"");
+                    Console.ReadKey();
+                }
+                return -1;
+            }
 
-			Console.WriteLine($"Starting: {app}");
+            var delay = args.Length >= 2 ? int.Parse(args[1]) : 5000;
 
-			Thread.Sleep(delay);
+            if (WaitForNewProcess(app))
+                return 0;
 
-			using (var p = new Process())
-			{
-				p.StartInfo.Verb = "runas";
-				p.StartInfo.FileName = app;
-				p.StartInfo.WorkingDirectory = Path.GetDirectoryName(app);
-				p.StartInfo.UseShellExecute = true;
-				p.Start();
-				Console.WriteLine($"PID: {p.Id}");
-				p.WaitForExit();
-			}
+            Console.WriteLine($"Starting: {app}");
 
-			WaitForNewProcess(app);
+            Thread.Sleep(delay);
 
-			return 0;
-		}
+            using (var p = new Process())
+            {
+                p.StartInfo.Verb = "runas";
+                p.StartInfo.FileName = app;
+                p.StartInfo.WorkingDirectory = Path.GetDirectoryName(app);
+                p.StartInfo.UseShellExecute = true;
+                p.Start();
+                Console.WriteLine($"PID: {p.Id}");
+                p.WaitForExit();
+            }
 
-		private static bool WaitForNewProcess(string app)
-		{
-			using (var process1 = GetNewProcess(app))
-				if (process1 == null)
-					return false;
-			while (true)
-			{
-				using (var process2 = GetNewProcess(app))
-				{
-					if (process2 == null)
-						return true;
-					Console.WriteLine($"PID: {process2.Id}");
-					process2.WaitForExit();
-				}
-			}
-		}
+            WaitForNewProcess(app);
 
-		private static Process? GetNewProcess(string app)
-		{
-			var processes = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(app));
-			if (processes.Length != 1)
-			{
-				foreach (var p in processes)
-					p.Dispose();
-				return null;
-			}
-			return processes[0];
-		}
-	}
+            return 0;
+        }
+
+        private static bool WaitForNewProcess(string app)
+        {
+            using (var process1 = GetNewProcess(app))
+                if (process1 == null)
+                    return false;
+            while (true)
+            {
+                using (var process2 = GetNewProcess(app))
+                {
+                    if (process2 == null)
+                        return true;
+                    Console.WriteLine($"PID: {process2.Id}");
+                    process2.WaitForExit();
+                }
+            }
+        }
+
+        private static Process? GetNewProcess(string app)
+        {
+            var processes = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(app));
+            if (processes.Length != 1)
+            {
+                foreach (var p in processes)
+                    p.Dispose();
+                return null;
+            }
+            return processes[0];
+        }
+    }
 }
